9b15b62d1b56272f5963c2fc365deea8e10ff50f
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-17 12:20:29 +0100

    beginning to get there. About to add sections

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function() { 
+
+  var $arrow_links_down = $(".arrow_down")
+  var $arrow_links_up = $(".arrow_up")
+
+
+  //////----- scrollTo (down)
+
+  $arrow_links_down.click( function(event) {
+    $.scrollTo(
+      $(this).attr("href"),
+      {
+        duration: 1000,
+        offset: { 'left':0, 'top':0 }
+      }
+    );
+  });
+
+  //////----- scrollTo (up)
+
+  $arrow_links_up.click( function(event) {
+    $.scrollTo(
+      $(this).attr("href"),
+      {
+        duration: 1000,
+        offset: { 'left':0, 'top':0 }
+      }
+    );
+  });
+
+});
