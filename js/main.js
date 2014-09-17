18c1d70fe74eab53abc81e167cc0ee1ab572eaac
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-17 12:36:02 +0100

    minor changes

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ $(function() {
   var $arrow_links_up = $(".arrow_up")
 
 
-  //////----- scrollTo (down)
+  //////----- scrollTo (up)
 
-  $arrow_links_down.click( function(event) {
+  $arrow_links_up.click( function(event) {
     $.scrollTo(
       $(this).attr("href"),
       {
@@ -16,9 +16,9 @@ $(function() {
     );
   });
 
-  //////----- scrollTo (up)
+  //////----- scrollTo (down)
 
-  $arrow_links_up.click( function(event) {
+  $arrow_links_down.click( function(event) {
     $.scrollTo(
       $(this).attr("href"),
       {
